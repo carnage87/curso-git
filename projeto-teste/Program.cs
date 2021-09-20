3fdfb626cff1326b1c582a9f2d9cf17d9d2b6c55
Walter Ary <walter.carnage@gmail.com>
2021-09-19 23:45:06 -0300

    Add files via upload

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
+        }
+    }
+}
