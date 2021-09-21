1c6c7123505b767d8e2013aee7aa35caf0b14441
carnage87 <walter.carnage@gmail.com>
2021-09-20 21:45:51 -0300

    Acrescentada a mensagem de despedida

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -9,6 +9,7 @@ namespace projeto_teste
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia");
+            Console.WriteLine("Tchau!");
         }
     }
 }
